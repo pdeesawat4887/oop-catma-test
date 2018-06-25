ee51048abcb4d83b7d5bbbec079d476291e29fc1
pdeesawat4887 <31108772+pdeesawat4887@users.noreply.github.com>
2018-06-25 09:03:05 +0700

    separate http and https connection.

--- header-info.py
+++ header-info.py
@@ -4,6 +4,7 @@ import requests
 url = 'http://www.squid-cache.org/'
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.170 Safari/537.36 OPR/53.0.2907.99'}
 response = requests.get(url, headers=headers)
+response.json()
 print response.status_code
 print response.headers
 # print response.encoding
@@ -19,3 +20,4 @@ print response.request
 # except Exception as ex:
 #     print "Could not connect to page."
 #     print
+
