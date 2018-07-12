9a6b9cb0b7b27d6346fae58da9030e43639d224d
pdeesawat4887 <31108772+pdeesawat4887@users.noreply.github.com>
2018-07-12 11:04:34 +0700

    test cgi python. Cheange 0.0.6

--- cgi-first.py
+++ cgi-first.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python
 import cgi
-print "Content-type: text/html\n\n"
 
-print "<!DOCTYPE html>"
-print "<html>"
-print "<head>"
-print "<title>Page Title</title>"
-print "</head>"
-print "<body>"
-
-print "<h1>This is a Heading</h1>"
-print "<p>This is a paragraph.</p>"
-
-print '<input class="input" type="text" placeholder="Text input">'
-
-print "</body>"
-print "</html>"
+print '<!DOCTYPE html>'
+print '<html>'
+print '<head>'
+print '<meta charset="utf-8">'
+print '<meta name="viewport" content="width=device-width, initial-scale=1">'
+print '<title>Hello Bulma!</title>'
+print '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">'
+print '<script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js"></script>'
+print '</head>'
+print '<body>'
+print '<section class="section">'
+print '<div class="container">'
+print '<h1 class="title">'
+print 'Hello World'
+print '</h1>'
+print '<p class="subtitle">'
+print 'My first website with <strong>Bulma</strong>!'
+print '</p>'
+print '</div>'
+print '</section>'
+print '</body>'
+print '</html>'
